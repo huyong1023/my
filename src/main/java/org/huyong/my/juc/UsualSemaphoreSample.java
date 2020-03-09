2ba6879cc0e605611e7eb5243f7cc31ba6a09d0b
Hu Yong <huyong@huaxiafinance.com>
2020-03-09 14:58:53 +0800

    juc

---
+++ src/main/java/org/huyong/my/juc/UsualSemaphoreSample.java
@@ -0,0 +1,44 @@
+package org.huyong.my.juc;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by yonghu on 2020/3/9.
+ */
+public class UsualSemaphoreSample {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Action...GO!");
+        Semaphore semaphore = new Semaphore(5);
+        for (int i = 0; i < 10; i++) {
+            Thread t = new Thread(new SemaphoreWorker(semaphore));
+            t.start();
+        }
+    }
+}
+class SemaphoreWorker implements Runnable {
+    private String name;
+    private Semaphore semaphore;
+    public SemaphoreWorker(Semaphore semaphore) {
+        this.semaphore = semaphore;
+    }
+    @Override
+    public void run() {
+        try {
+            log("is waiting for a permit!");
+            semaphore.acquire();
+            log("acquired a permit!");
+            log("executed!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            log("released a permit!");
+            semaphore.release();
+        }
+    }
+    private void log(String msg){
+        if (name == null) {
+            name = Thread.currentThread().getName();
+        }
+        System.out.println(name + " " + msg);
+    }
+}
---
+++ src/main/java/org/huyong/my/lock/DeadLockSample.java
@@ -0,0 +1,30 @@
+package org.huyong.my.lock;
+
+/**
+ * Created by yonghu on 2020/3/9.
+ */
+public class DeadLockSample extends Thread {
+    private String first;
+    private String second;
+
+    public DeadLockSample(String name, String first, String second) {
+        super(name);
+        this.first = first;
+        this.second = second;
+    }
+
+    public void run() {
+        synchronized (first) {
+            System.out.println(this.getName() + " obtained: " + first);
+            try {
+                Thread.sleep(1000L);
+                synchronized (second) {
+                    System.out.println(this.getName() + " obtained: " + second);
+                }
+            } catch (InterruptedException e) {
+                // Do nothing
+            }
+        }
+    }
+
+}
