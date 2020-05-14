bfad6d84bcf107a6954fafd50e221f0a4c6a1906
Hu Yong <huyong@huaxiafinance.com>
2020-05-14 11:01:24 +0800

    add test

---
+++ src/test/java/org/huyong/my/juc/AppTest.java
@@ -0,0 +1,96 @@
+package org.huyong.my.juc;
+
+import org.junit.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by yonghu on 2020/3/9.
+ */
+public class AppTest {
+    @Test
+    public void cachedThred() {
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            try {
+                Thread.sleep(index * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            cachedThreadPool.execute(new Runnable() {
+
+                @Override
+                public void run() {
+                    System.out.println(index);
+                }
+            });
+        }
+    }
+
+    @Test
+    public void fixedThread() {
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(3);
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            fixedThreadPool.execute(new Runnable() {
+
+                @Override
+                public void run() {
+                    try {
+                        System.out.println(index);
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void schedultdThreadPool(){
+        ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(5);
+        scheduledThreadPool.schedule(new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println("delay 3 seconds");
+            }
+        }, 3, TimeUnit.SECONDS);
+    }
+
+
+
+    @Test
+    public void singleThread(){
+        ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            singleThreadExecutor.execute(new Runnable() {
+
+                @Override
+                public void run() {
+                    try {
+                        System.out.println(index);
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ src/test/java/org/huyong/my/lock/AppTest.java
@@ -0,0 +1,51 @@
+package org.huyong.my.lock;
+
+import org.junit.Test;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by yonghu on 2020/3/9.
+ */
+public class AppTest {
+
+    @Test
+    public  void test() throws InterruptedException {
+
+        ThreadMXBean mbean = ManagementFactory.getThreadMXBean();
+        Runnable dlCheck = new Runnable() {
+
+            @Override
+            public void run() {
+                long[] threadIds = mbean.findDeadlockedThreads();
+                if (threadIds != null) {
+                    ThreadInfo[] threadInfos = mbean.getThreadInfo(threadIds);
+                    System.out.println("Detected deadlock threads:");
+                    for (ThreadInfo threadInfo : threadInfos) {
+                        System.out.println(threadInfo.getThreadName());
+                    }
+                }
+            }
+        };
+
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        // 稍等5秒，然后每10秒进行一次死锁扫描
+        scheduler.scheduleAtFixedRate(dlCheck, 5L, 10L, TimeUnit.SECONDS);
+        // 死锁样例代码…
+
+
+        String lockA = "lockA";
+        String lockB = "lockB";
+        DeadLockSample t1 = new DeadLockSample("Thread1", lockA, lockB);
+        DeadLockSample t2 = new DeadLockSample("Thread2", lockB, lockA);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+}
